7284fb06f43f96954e0cd1b36f2f044d97e0377b
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-09 10:32:23 -0300

    :sparkles: Create the splash screen

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/images/LogoCollabChat.svg";
+import "./logo.css";
+
+function Logo() {
+  return <img className="logo" src={logo} alt="Logo da ChatCollab" />;
+}
+
+export default Logo;
